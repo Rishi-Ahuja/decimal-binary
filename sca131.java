2e39cc48896fcc3c82db58f167d7a64e81e8824b
Rishi Ahuja <69950416+Rishi-Ahuja@users.noreply.github.com>
2022-06-08 00:15:06 +0530

    Add files via upload

---
+++ sca131.java
@@ -0,0 +1,21 @@
+//program to convert decimal to binary
+import java.util.*;
+public class sca131
+{
+    public static void main(String[] args)
+    {
+        Scanner user = new Scanner(System.in);
+        System.out.println("Enter your input");
+        long num = user.nextLong();
+        long binary = 0, x= 0, count = 0;
+        while (num != 0 )
+        {
+            x = num % 2;
+            double y = Math.pow(10, count);
+            num /= 2;
+            binary += x * y;
+            count++;
+        }
+        System.out.println(binary);
+    }
+}
